096924c692ee1fa7652f7820cff99705754e65ca
Marcus Collins <mjdj27123@gmail.com>
2026-09-10 19:38:07 +0100

    Add Day 22 2017

---
+++ AOC 2017/Day 22 Part 2 2017.py
@@ -0,0 +1,46 @@
+from time import time
+t1 = time()
+FOLDER_PATH = "C:/Users/mjdj2/OneDrive/Documents/AOC/AOC 2017/"
+FILE_NAME = "Day 22 2017.txt"
+# FILE_NAME = "Day 22 2017 test.txt"
+
+file = open(FOLDER_PATH + FILE_NAME, "r")
+data = [x.strip() for x in file.readlines()]
+file.close()
+
+WIDTH, HEIGHT = len(data[0]), len(data)
+
+infected = set() # Store as (x, y)
+flagged = set()
+weakened = set()
+
+for r in range(HEIGHT):
+    for c in range(WIDTH):
+        if data[r][c] == "#":
+            infected.add((c-WIDTH//2, r-HEIGHT//2))
+
+currPos = (0, 0) # Store as (x, y)
+currDir = (0, -1)
+numInfections = 0
+
+LEFT = {(0, -1): (-1, 0), (-1, 0): (0, 1), (0, 1): (1, 0), (1, 0): (0, -1)}
+RIGHT = {(0, -1): (1, 0), (1, 0): (0, 1), (0, 1): (-1, 0), (-1, 0): (0, -1)}
+REVERSE = {(0, -1): (0, 1), (0, 1): (0, -1), (1, 0): (-1, 0), (-1, 0): (1, 0)}
+
+for _ in range(10_000_000):
+    currDir = RIGHT[currDir] if currPos in infected else currDir if currPos in weakened else REVERSE[currDir] if currPos in flagged else LEFT[currDir]
+    if currPos in infected:
+        infected.remove(currPos)
+        flagged.add(currPos)
+    elif currPos in weakened:
+        weakened.remove(currPos)
+        infected.add(currPos)
+        numInfections += 1
+    elif currPos in flagged:
+        flagged.remove(currPos)
+    else:
+        weakened.add(currPos)
+    currPos = (currPos[0]+currDir[0], currPos[1]+currDir[1])
+
+print(numInfections)
+print(f"Time Taken: {time()-t1:.2f}s")
